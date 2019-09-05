439ce42fa0b956849a1fd629c4b5b6a9c4e10940
s.s <jnshiba.621@gmail.com>
2019-09-05 07:39:40 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'MicropostsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -21,8 +19,9 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function(){
+Route::group(['middleware' => 'auth'], function(){
     Route::resource('users', 'UserController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });   
 
 
